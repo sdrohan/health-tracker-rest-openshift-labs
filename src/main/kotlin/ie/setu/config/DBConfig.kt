3effcfc566a3dcdb96ebf7cfb3cb64903bc8eea8
Siobhan Drohan <sdrohan@wit.ie>
2025-04-29 15:47:43 +0100

    javalin get remote port from openshift

--- src/main/kotlin/ie/setu/config/DBConfig.kt
+++ src/main/kotlin/ie/setu/config/DBConfig.kt
@@ -11,26 +11,30 @@ class DbConfig {
 
     fun getDbConnection(): Database {
 
-        val PGHOST = System.getenv("POSTGRESQL_HOST") ?: "localhost"
+        val rawHost = System.getenv("POSTGRESQL_HOST") ?: "localhost"
+        val PGHOST = rawHost.substringAfterLast("://")
         val PGPORT = System.getenv("POSTGRESQL_SERVICE_PORT") ?: "5432"
         val PGDATABASE = System.getenv("POSTGRESQL_DATABASE") ?: "defaultdb"
         val PGUSER = System.getenv("POSTGRESQL_USER") ?: "defaultuser"
         val PGPASSWORD = System.getenv("POSTGRESQL_PASSWORD") ?: "defaultpass"
 
         // JDBC connection string format
-        val dbUrl = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+        val dbUrl = "jdbc:postgresql://$rawHost:$PGPORT/$PGDATABASE"
 
         try {
             logger.info { "Starting DB Connection...\nURL: $dbUrl" }
+            logger.info { "rawHost...: $rawHost" }
+            logger.info { "pghost...: $PGHOST" }
             dbConfig = Database.connect(
                 url = dbUrl,
                 driver = "org.postgresql.Driver",
                 user = PGUSER,
                 password = PGPASSWORD
             )
-            logger.info { "DB Connected Successfully to $PGDATABASE at $PGHOST:$PGPORT" }
+            logger.info { "DB Connected Successfully to $PGDATABASE at $rawHost:$PGPORT" }
         } catch (e: PSQLException) {
             logger.error(e) { "Error in DB Connection: ${e.message}" }
+            logger.info { "Env vars used: host=$PGHOST, port=$PGPORT, user=$PGUSER, db=$PGDATABASE" }
         }
 
         return dbConfig
--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -5,13 +5,13 @@ import io.javalin.Javalin
 
 class JavalinConfig {
 
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        error(404) { ctx -> ctx.json("404 - Not Found") }
+    }
+
     fun startJavalinService(): Javalin {
-
-        val app = Javalin.create().apply {
-            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-            error(404) { ctx -> ctx.json("404 - Not Found") }
-        }.start(7002)
-
+        app.start(getRemoteAssignedPort())
         registerRoutes(app)
         return app
     }
@@ -23,6 +23,18 @@ class JavalinConfig {
         app.delete("/api/users/{user-id}", HealthTrackerController::deleteUser)
         app.patch("/api/users/{user-id}", HealthTrackerController::updateUser)
         app.get("/api/users/email/{email}", HealthTrackerController::getUserByEmail)
+    }
+
+    private fun getRemoteAssignedPort(): Int {
+        val remotePort = System.getenv("PORT")
+        return if (remotePort != null) {
+            Integer.parseInt(remotePort)
+        } else 8080
+    }
+
+    fun getJavalinService(): Javalin {
+        registerRoutes(app)
+        return app
     }
 
 }
