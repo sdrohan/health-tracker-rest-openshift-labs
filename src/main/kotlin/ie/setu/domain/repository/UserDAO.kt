fd4fea4c2ab87642581ae3b850c2a91a613ea657
Siobhan Drohan <sdrohan@wit.ie>
2025-04-30 14:37:52 +0100

    full crud on user table

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -3,8 +3,12 @@ package ie.setu.domain.repository
 import ie.setu.domain.User
 import ie.setu.domain.db.Users
 import ie.setu.utils.mapToUser
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
 
 class UserDAO {
 
@@ -25,17 +29,36 @@ class UserDAO {
         }
     }
 
-
     fun save(user: User){
+        transaction {
+            Users.insert {
+                it[name] = user.name
+                it[email] = user.email
+            }
+        }
     }
 
     fun findByEmail(email: String) :User?{
-        return null
+        return transaction {
+            Users.selectAll().where { Users.email eq email }
+                .map{mapToUser(it)}
+                .firstOrNull()
+        }
     }
 
-    fun delete(id: Int) {
+    fun delete(id: Int):Int{
+        return transaction{
+            Users.deleteWhere{ Users.id eq id }
+        }
     }
 
     fun update(id: Int, user: User){
+        transaction {
+            Users.update ({
+                Users.id eq id}) {
+                it[name] = user.name
+                it[email] = user.email
+            }
+        }
     }
 }
