29253860f4c3b71bfda2fbc56cca17d16094ee44
Siobhan Drohan <sdrohan@wit.ie>
2025-06-04 16:21:27 +0100

    Add dokka to maven

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -10,6 +10,9 @@ import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 
+/**
+ * Manages the database transactions and returns the results of the transactions
+ */
 class UserDAO {
 
     fun getAll(): ArrayList<User> {
@@ -29,6 +32,10 @@ class UserDAO {
         }
     }
 
+    /**
+     * Adds a [user] to the Users table.
+     * @return the id of the user following the add.
+     */
     fun save(user: User) : Int?{
         return transaction {
             Users.insert {
